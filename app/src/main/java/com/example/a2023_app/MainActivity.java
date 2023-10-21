12f6d2d4b823070825c3b14809b38488de97b45d
Qwert <hfl.r@mail.ru>
2023-10-21 18:37:50 +0300

    Good1

--- app/src/main/java/com/example/a2023_app/MainActivity.java
+++ app/src/main/java/com/example/a2023_app/MainActivity.java
@@ -3,23 +3,20 @@ package com.example.a2023_app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button startButton = (Button) findViewById(R.id.key_start);  //
-        startButton.setOnClickListener((view ->
-                startButton.setBackgroundResource(R.drawable.ic_launcher_background)));
-        try {
-            Intent intent = new Intent(MainActivity.this, On_Start_MainActivity.class);
-            startActivity(intent);
-            finish();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    }
+    public void OnClick(View view) {
+        Intent intent = new Intent(MainActivity.this, On_Start_MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/a2023_app/On_Start_MainActivity.java
+++ app/src/main/java/com/example/a2023_app/On_Start_MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a2023_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class On_Start_MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class On_Start_MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.on_start_activity);
+
+
     }
 }
