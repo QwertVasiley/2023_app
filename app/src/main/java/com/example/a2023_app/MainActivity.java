c9b748f13c3803bfc343b30d1c1e79991f4149c6
Qwert <hfl.r@mail.ru>
2023-10-21 18:19:54 +0300

    fignya

--- app/src/main/java/com/example/a2023_app/MainActivity.java
+++ app/src/main/java/com/example/a2023_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a2023_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button startButton = (Button) findViewById(R.id.key_start);  //
+        startButton.setOnClickListener((view ->
+                startButton.setBackgroundResource(R.drawable.ic_launcher_background)));
+        try {
+            Intent intent = new Intent(MainActivity.this, On_Start_MainActivity.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/a2023_app/On_Start_MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a2023_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class On_Start_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.on_start_activity);
+    }
+}
